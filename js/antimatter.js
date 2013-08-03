3f894b67026d137e10f3a996cd79daca84d903c4
OmeGak <omegak@gmail.com>
2013-08-03 15:20:29 +0200

    three.js camera rotation

--- js/antimatter.js
+++ js/antimatter.js
@@ -4,26 +4,32 @@ var renderer;
 
 function init() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.x = 20;
+    camera.position.y = 7;
+    camera.position.z = 20;
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    document.body.appendChild(renderer.domElement);
 
-    geometry = new THREE.CubeGeometry(1,1,1);
-    material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    cube = new THREE.Mesh( geometry, material );
+    geometry = new THREE.CubeGeometry(10,10,10);
+    material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    cube = new THREE.Mesh(geometry, material);
     scene.add( cube );
+}
 
-    camera.position.z = 5;
+function animate() {
+    var timer = Date.now() * 0.0002;
+    camera.position.x = Math.cos(timer) * 20;
+    camera.position.z = Math.sin(timer) * 20;
+    camera.lookAt(scene.position);
 }
 
 function render() {
     requestAnimationFrame(render);
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
+    animate();
     renderer.render(scene, camera);
 }
 
