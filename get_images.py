f4a543094fbeeced994bfcd9da9e14ae29c6d803
Daniel Lombraña González <teleyinex@gmail.com>
2013-08-03 13:55:28 +0200

    Create tasks for Antimatter project
    This commit reads a folder with the images, creates a task per viewid. The
    user has to pass a url where the images will be accessible.
    
    The URL lacks the zoom level and the extension, so the script will create
    for the task an info field like this:
    
    url = 'http://domain/image_viewid_ifr
    
    The template will have to fill in the zoom and .jpg extension

---
+++ get_images.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of PyBOSSA.
+#
+# PyBOSSA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBOSSA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyBOSSA.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import string
+
+
+def get_antimatter_pictures(folder, url):
+    """
+    Gets public photos from a folder
+    """
+    tasks = dict()
+    photos = []
+    for f in os.listdir(folder):
+        if f.endswith(".jpg"):
+            image, viewid, zoom = f.split("_")
+            if tasks.get(viewid) is None:
+                _url = "%s/image_%s_ifr" % (url, viewid)
+                tasks[viewid] = _url
+    for k in tasks.keys():
+        photos.append(tasks[k])
+    return photos
